288c7a03488275e3170aa7866a7273e46e532e4b
AuBrille <99645822+AuBrille@users.noreply.github.com>
2024-10-30 17:42:34 +0100

    test ci failed on automation

--- frontend/src/app/recette/page.tsx
+++ frontend/src/app/recette/page.tsx
@@ -5,6 +5,7 @@ export default function Page() {
     <div className="mb-8 bg-green-500 py-20 text-center text-white">
       <h1 className="text-4xl font-bold">Recette</h1>
       <p className="mt-2 text-xl">Controllez vos apports énergétiques</p>
+      {test}
     </div>
   );
 }
